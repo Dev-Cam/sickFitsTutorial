361c904ee776797e8ca568d37a4ba2a5304b1827
Cameron <reid.camerond@gmail.com>
2023-06-29 14:49:36 +1000

    nextjs updates

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ad2742074e73164f29b"; }
+/******/ 		__webpack_require__.h = function() { return "f0702d38733962f20051"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
